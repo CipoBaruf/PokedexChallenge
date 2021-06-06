fd6237da13ac4f83473f6b2102db2cf5bbb70352
Ezequiel Nallib Baruf <cipobaruf@pop-os.localdomain>
2021-06-06 19:45:50 -0300

    implemented languages translations

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import { getPokemons } from '../API.js'
+import { getPokemons, getPokemon } from '../API.js'
 
 export default function Home() {
 
@@ -8,30 +8,52 @@ export default function Home() {
 
     const [newCall, setNewCall] = useState('');
 
-    const [prevNext, setPrevNext] = useState({prev: '', next: ''})
+    const [prevNext, setPrevNext] = useState({ prev: '', next: '' })
 
-    const [loading, setLoading]= useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const [languageOption, setLanguageOption] = useState('es');
 
     useEffect(() => {
         setLoading(true);
+        setPokemons([])
         getPokemons(newCall).then((response) => {
-            setPokemons(response.data.results);
+            response.data.results.forEach((result) => {
+                getPokemon(result.url).then((res)=>{
+                    console.log(res);
+                    res.data.names.forEach((names)=>{
+                        if(names.language.name === languageOption){
+                            setPokemons(old =>[...old, names.name ])
+                        }
+                    })
+                })
+            })
             setPrevNext({
                 prev: response.data.previous,
                 next: response.data.next,
             })
             setLoading(false);
-        })
-    }, [newCall])
+        }
+        )
+    }, [languageOption, newCall])
 
     return (
-        <div className="">
-            {!loading && pokemons ? pokemons.map((pokemon, index) =>
-                <h1 key={index}>{pokemon.name}</h1>
-            )
-                :
-                <p>Loading</p>
-            }
+        <div className="p-4">
+            <div className="flex items-cener justify-end">
+                <select value={languageOption} onChange={(e) => setLanguageOption(e.target.value)}>
+                    <option value="es">Spanish</option>
+                    <option value="en">English</option>
+                    <option value="fr">French</option>
+                </select>
+            </div>
+            <div className="h-48">
+                {!loading && pokemons ? pokemons.map((pokemon, index) =>
+                    <h1 key={index}>{pokemon}</h1>
+                )
+                    :
+                    <p>Loading</p>
+                }
+            </div>
             <div className="flex items-center justify-center space-x-5">
                 <button disabled={prevNext.prev === null} onClick={() => setNewCall(prevNext.prev)} className="btn">Previous</button>
                 <button disabled={prevNext.next === null} onClick={() => setNewCall(prevNext.next)} className="btn">Next</button>
